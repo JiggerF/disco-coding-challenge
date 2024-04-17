1c103869b8095ab0056ef00488965f309ca5d609
JiggerF <tekton3423@developer.com>
2024-04-17 17:59:24 +1000

    add tests to verify pricing slider behaviour

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # disco-coding-challenge
 Disco's QA Engineering assessment
+
+## How to Run Locally
+```
+    $ npx playwright test pricingPage.spec.js --project='chromium' --reporter=list
+```
--- tests/pricingPage.spec.js
+++ tests/pricingPage.spec.js
@@ -1,6 +1,12 @@
-// @ts-check
 const { test, expect } = require('@playwright/test')
 const { PLAN_PACKAGES } = require('../common/planPackagesConfig')
+
+// TODO: Refactor to use money datatype
+const plusMonthlyChargePerUserInDollars = 15
+const plusMonthlyChargePerTracksInDollars = 6
+const proMonthlyChargePerUserInDollars = 25
+const plusAnnualChargePerUserInDollars = 13.50
+const proAnnualChargePerUserInDollars = 22.50
 
 // Define Locators
 let litePriceLocator = 'div.PricingPlans > div:nth-child(1) > div > div.PricingPlan_ContentWrapper > div > div > div.PricingPlan_PriceContainer > div.PricingPlan_PriceWrapper > div.Price'
@@ -53,7 +59,7 @@ test.describe('Pricing Cycle - Default', () => {
   })
 
   test('select pay Annually should update all package pricing correctly', async ({ page }) => {
-    // Action - Select Pay Annually slider
+    // Action - Select Pay Annually button
     await page.locator('div').filter({ hasText: /^Pay monthlyPay Annually$/ }).locator('div').nth(2).click();
 
     // Assert LITE => $9 per month for 1 user and 500 tracks
@@ -74,20 +80,35 @@ test.describe('Pricing Cycle - Default', () => {
 })
 
 
-test.describe('Pricing Page - with nondefault pricing range', () => {
+test.describe('Pricing Page - pricing slider change', () => {
   test('should update price when pricing slider is changed', async ({ page }) => {
+    // Assert slider is defaulted to monthly
+    await expect(page.locator('.div.Toggle.Toggle--Checked')).toHaveCount(0)
 
     // PLUS
     let userLocatorSlider = 'div:nth-child(2) > .PricingRange > .rc-slider'
     let fileLocatorSlider = 'div:nth-child(3) > .PricingRange > .rc-slider'
-    
-    await changeSlider(page, userLocatorSlider, .3) // users
+
+    // $99 per month for 1 users and 15K Tracks
+    await changeSlider(page, userLocatorSlider, 0) // user slider set to zero, selecting the selector is flaky
     await changeSlider(page, fileLocatorSlider, .5) // tracks
 
+    console.log(await page.locator(plusPriceLocator).textContent())
+    console.log(await page.locator(plusPriceSubLocator).textContent())
+
     // Assert UI calculated price
-    
+    let uiPricingPlanTotal = (await page.locator(plusPriceLocator).textContent())?.replace(/\D/g,'')
+    let uiSubText = await page.locator(plusPriceSubLocator).textContent()
+
+    let expectedTotalUserCost = calcUserTotalCharge(1, plusMonthlyChargePerUserInDollars)
+    let expectedTotalTrackCost = calcTracksTotalCharge(15000, plusMonthlyChargePerTracksInDollars)
+
+    console.log(expectedTotalUserCost)
+    console.log(expectedTotalTrackCost)
+    expect(parseInt(uiPricingPlanTotal)).toBe(expectedTotalUserCost + expectedTotalTrackCost)
   })
 })
+
 
 async function changeSlider(page, locator, percentage) {
   page.mouse.wheel(0, 400)
@@ -108,6 +129,10 @@ async function changeSlider(page, locator, percentage) {
   await page.mouse.down()
   await sliderTrack.hover({ force: true, position: { x: sliderOffsetWidth * percentage, y: sliderOffsetHeight / 2 } })
   await page.mouse.up()
+}
+
+function calcUserTotalCharge(numUser, basePrice) {
+  return numUser * basePrice
 }
 
 function calcTracksTotalCharge(numTracks, basePrice) {
